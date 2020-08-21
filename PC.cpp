97bf3e716c00fc7f60eb318a58c0774883dff4d7
Miguel Rojas <miguel@Miguels-MacBook-Air.local>
2020-08-21 15:51:12 -0600

    Arreglo de errores

--- PC.cpp
+++ PC.cpp
@@ -21,21 +21,45 @@ void PC::ping(string ip_buscado){
     string delimitador = ".";
     string cadena_temporal = direccion_IP;
     int posicion = 0;
-    vector<string> numeros_direccion_ip;
-    
+    vector<string> valores_direccion_ip;
+    //Recorrer cadena y agregar numeros a lista de string
     while((posicion = cadena_temporal.find(delimitador))!= string::npos){
-        numeros_direccion_ip.push_back(cadena_temporal.substr(0, posicion));
+        valores_direccion_ip.push_back(cadena_temporal.substr(0, posicion));
         cadena_temporal.erase(0, posicion + delimitador.length());
     }
+    valores_direccion_ip.push_back(cadena_temporal);
 
-    numeros_direccion_ip.push_back(cadena_temporal);
-
-    for (int i = 0; i < numeros_direccion_ip.size(); i++){
-        cout << numeros_direccion_ip[i] << endl;
+    //Convertir valores de string obtenidos a int
+    int numeros_direccionIP_usuario[4];
+    for (int i = 0; i < 4; i++){
+        int entero_temporal = std::stoi(valores_direccion_ip[i]);
+        numeros_direccionIP_usuario[i] = entero_temporal;
     }
+
+    //Repetir proceso con la direccion ip buscada
+    string cadena_buscada_temporal = ip_buscado;
+    int posicion_2 = 0;
+    vector<string> valores_direccion_buscada;
     
+    while((posicion_2 = cadena_buscada_temporal.find(delimitador))!= string::npos){
+        valores_direccion_buscada.push_back(cadena_buscada_temporal.substr(0, posicion_2));
+        cadena_buscada_temporal.erase(0, posicion_2 + delimitador.length());
+    }
+    valores_direccion_buscada.push_back(cadena_temporal);
+    int numeros_direccionIP_buscada[4];
+    //Convertir valores de string obtenidos a int
+    for (int i = 0; i < 4; i++){
+        int entero_temporal = std::stoi(valores_direccion_buscada[i]);
+        numeros_direccionIP_buscada[i] = entero_temporal;
+    }
+
+    //Convertir direccion ip de usuario a binario
+
     
-    
-    
+
+
+
+
 }
+//Destructor
 PC::~PC(){}
